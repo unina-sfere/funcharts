4741b4b3f006c9c912e77ac4c62d426d582ef868
capezza <christian.capezza@gmail.com>
2021-03-15 20:22:10 +0100

    updated website

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -3,3 +3,18 @@ knitr::knit("vignettes/centofanti2020.Rmd.orig", "vignettes/centofanti2020.Rmd")
 knitr::knit("vignettes/mfd.Rmd.orig", "vignettes/mfd.Rmd")
 
 devtools::build_vignettes()
+
+
+## After building the website with pkgdown, rename files to get the right image paths
+files <- list.files("docs/articles", full.names = TRUE)
+files_html <- files[grepl(".html", files)]
+
+for (ff in files_html) {
+  text <- readLines(ff)
+  new_text <- gsub("../../../R%20packages/funcharts/vignettes/", "", text)
+  writeLines(new_text, ff)
+}
+
+
+
+
