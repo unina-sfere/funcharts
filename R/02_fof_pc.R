1339737d665c97db8e3f8d49480ecaa4140ff4c7
capezza <christian.capezza@gmail.com>
2022-02-22 13:01:48 +0100

    fix

--- R/02_fof_pc.R
+++ R/02_fof_pc.R
@@ -261,20 +261,23 @@ fof_pc <- function(mfdobj_y,
   B <- mod$coefficients
   B <- as.matrix(B)
 
-  pp <- dim(x_pca$harmonics$coefs)[3]
   Gx <- x_pca$harmonics$coefs[, components_x, ]
-  Gx <- do.call(rbind, lapply(1:pp, function(jj) Gx[, , jj]))
+  Gx <- do.call(rbind, lapply(1:n_var_x, function(jj) Gx[, , jj]))
   Gy <- y_pca$harmonics$coefs[, components_y, 1, drop = FALSE]
   Gy <- matrix(as.numeric(Gy), nrow = dim(Gy)[1], ncol = dim(Gy)[2])
   beta_coefs <- Gx %*% B %*% t(Gy)
   k1 <- x_pca$harmonics$basis$nbasis
   k2 <- y_pca$harmonics$basis$nbasis
-  beta_coefs <- array(as.numeric(t(beta_coefs)), dim = c(k2, k1, 1, pp))
+  beta_coefs <- array(as.numeric(t(beta_coefs)), dim = c(k2, k1, 1, n_var_x))
   beta_coefs <- aperm(beta_coefs, perm = c(2, 1, 3, 4))
+  bifdnames <- list(x_pca$harmonics$basis$names,
+                    y_pca$harmonics$basis$names,
+                    "",
+                    x_pca$harmonics$fdnames[[3]])
   beta_fd <- bifd(beta_coefs,
                   x_pca$harmonics$basis,
                   y_pca$harmonics$basis,
-                  phi_st$bifdnames)
+                  bifdnames)
 
   y_scores_hat <- predict(mod)
 
